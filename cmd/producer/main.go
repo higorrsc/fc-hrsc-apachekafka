8f1d100ca3b5d3f6c2d266cc13e6c7a8c71f1dfc
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-02-11 18:46:23 -0300

    feat: implement publish message

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	fmt.Println("Hello Go")
+	producer := NewKafkaProducer()
+	Publish("mensagem", "teste", producer, nil)
+	producer.Flush(1000)
 }
 
+// NewKafkaProducer returns a new Kafka producer.
+//
+// The returned producer is configured to connect to the broker at
+// apache-kafka-kafka-1:9092. If the connection to the broker fails, the
+// error is logged with log.Println.
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
 		"bootstrap.servers": "apache-kafka-kafka-1:9092",
@@ -20,4 +26,32 @@ func NewKafkaProducer() *kafka.Producer {
 		log.Println(err.Error())
 	}
 	return p
+}
+
+// Publish sends a message to a specified Kafka topic using the provided producer.
+//
+// Parameters:
+// - msg: the message to be sent.
+// - topic: the Kafka topic to which the message will be published.
+// - producer: the Kafka producer used to send the message.
+// - key: an optional key for partitioning the message.
+//
+// Returns an error if the message could not be produced.
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value: []byte(msg),
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Key: key,
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
