927b3b7e4acdfdbedcf8887cefaed98e574b1ba6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-02-11 18:37:01 -0300

    feat: add kafka producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	fmt.Println("Hello Go")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "apache-kafka-kafka-1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/higorrsc/fc-hrsc-apache-kafka
 
 go 1.23.2
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
